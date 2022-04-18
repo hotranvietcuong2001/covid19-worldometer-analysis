12b6665ef4950404dc0fac49d4f428b8e8743983
hotranvietcuong <hotranvietcuong2001@gmail.com>
2022-04-18 19:58:13 +0700

    add file crawl data

---
+++ crawl_data.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+import csv
+import os.path
+
+
+
+url = 'https://www.worldometers.info/coronavirus/'
+
+
+data = requests.get(url)
+soup = BeautifulSoup(data.content)
+
+
+def string_to_int_with_comma(cell):
+    return cell.replace(',', '')
+
+
+def crawl_covid_today(file_name='covid_data'):
+    header = ['Date', 'Country', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths', 
+                'Total Recovered', 'New Recovered', 'Active Cases', 'Serious Critical',
+                'Tot Cases/1M pop', 'Deaths/1M pop', 'Total Tests', 'Tests/1M pop', 'Population']
+
+    data = []
+
+    table = soup.html.find('table', attrs={'id':'main_table_countries_today'})
+    rows_table = table.find('tbody').find_all('tr')
+
+    for row in rows_table:
+        cells = row.find_all('td')
+        cells = [ele.text.strip() for ele in cells]
+        if cells[0] != '':
+            country_info = cells[1:15]
+            country_info[1:] = list(map(string_to_int_with_comma, country_info[1:]))
+            date = datetime.date(datetime.now())
+            country_info.insert(0, date)
+            data.append(country_info)
+
+    is_exist = True
+
+    if not os.path.isfile(file_name):
+        is_exist = False
+    
+    with open(file_name, 'a', newline='') as fo:
+        write = csv.writer(fo)
+
+        if not is_exist:
+            write.writerow(header)
+        write.writerows(data)
+
+
+
+if __name__ == '__main__':
+    crawl_covid_today('data/covid_data.csv')
